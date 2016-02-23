186b63a7ef26ec4df1ae3e35c9cc595670ce9117
Daniel Klabbers <daniel+hyn@klabbers.email>
2016-02-23 23:08:06 +0100

    attempt to fix filename on download

--- routes.php
+++ routes.php
@@ -4,6 +4,6 @@ Route::get('/', function () {
     return Redirect::away('https://hyn.readme.io/docs/installation#2-using-the-automatic-installer');
 });
 
-Route::get('/download', [
+Route::get('/download/installer.sh', [
     'uses' => 'Hyn\Installer\Http\Controllers\DownloadController@generate'
 ]);
